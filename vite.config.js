2e6fe2defc1c1059d1bf85572d2a54d5d85fcc93
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-06-06 15:55:29 +0200

    add external css-js landing

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/landing.css',
                 'resources/js/app.js',
+                'resources/js/landing.js',
+
             ],
             refresh: true,
         }),
