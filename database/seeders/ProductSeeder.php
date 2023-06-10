3a82e1718e72f1766f3f5d90be916e10ea217ae6
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-06-10 18:07:56 +0200

    Product Seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,34 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        Product::create([
+            'name' => 'Scullyon Black 2.0',
+            'price' => 25,
+            'stock' => 50,
+            'description' => 'Basic hoodie with a kangaroo pocket,
+                                a drawstring hood.With a soft brushed inside.', 
+            'category_id' => 1, // 1 = Hoodies
+        ],[
+            'name' => 'Try It Hoodie',
+            'price' => 27.5,
+            'stock' => 50,
+            'description' => 'Exclusive desing for the Try It event.',
+            'category_id' => 1,
+        ],
+        [
+            'name' => 'Please WaIT T-Shirt',
+            'price' => 17.5,
+            'stock' => 35,
+            'description' => 'Basic T-Shirt with a round neck and short sleeves.
+                                Exclusive desing for the first drop.',      
+            'category_id' => 2,// 2 = T-Shirts
+        ],
+        [
+            'name' => 'Flowher Hoodie',
+            'price' => 45,
+            'stock' => 55,
+            'description' => 'A hoodie with a diferent flow or flower.Who knows?',
+            'category_id' => 1,
+        ]);
     }
 }
